5aeaac4be5a427b0e5dfc173ad32cf97364631ab
Firman Ismail <akayasha99@gmail.com>
2022-02-11 22:09:16 +0700

    tugas 26 js

---
+++ tugas26.js
@@ -0,0 +1,13 @@
+let x = [ 2, 39, 76, 50, 9, 7, 41, 2, 24, 1, 16];
+function cek() {
+  x.forEach((x, i) => {
+    if (i = isFinite(x)){
+      console.log("Angka",x ,"NOT INFINITY");
+}
+    else {
+      console.log("xx")
+  }
+
+  });
+}
+cek()
